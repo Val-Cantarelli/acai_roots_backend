cf804c71ea0fe6738aae278c0cc68ba4fc24cb2d
Valdielen Casarin <valcanterellic@gmail.com>
2023-08-15 14:39:04 -0400

    Second commit

--- src/main/java/com/valdielencasarin/schedulemanagement/SchedulemanagementApplication.java
+++ src/main/java/com/valdielencasarin/schedulemanagement/SchedulemanagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SchedulemanagementApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulemanagementApplication.class, args);
 	}
